112b27ff8b8936d3ede8656b3597b9d90ca9dcb7
MekaiJ <mekai1026@gmail.com>
2026-01-23 17:58:06 -0800

    populate audioList.h

--- include/audioList.h
+++ include/audioList.h
@@ -1,4 +1,18 @@
 #pragma once
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
 
+Node* createNode(int data);
+void insertAtBeginning(Node** head, int data);
+void insertAtEnd(Node** head, int data);
+void insertAtPosition(Node** head, int data, int position);
+void deleteAtBeginning(Node** head);
+void deleteAtEnd(Node** head);
+void deleteAtPosition(Node** head, int position);
+void printListForward(Node* head);
+void printListReverse(Node* head);
 
 
